21176151b6a26eb652e24ef2fd71cdda891678c8
Matt Izzo (RIT Student) <mmi4105@rit.edu>
2021-10-02 19:55:21 -0400

    Proximity detection for InteractObjects
    First I added the interactObject script to the interact object prefab.
    
    Then I added the interact object prefab to the scene
    
    Then I made it so the player can actually move
    
    Finally I made it so that if the player is within .5 units of the interact object a console message is displayed.

--- OnHalloweenLight/Assets/Scripts/InteractObject.cs
+++ OnHalloweenLight/Assets/Scripts/InteractObject.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class InteractObject : MonoBehaviour, IInteractable
 {
+    [SerializeField] private Player player;
+    private float xDifference;
+    private float yDifference;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,7 +18,67 @@ public class InteractObject : MonoBehaviour, IInteractable
     // Update is called once per frame
     void Update()
     {
-        
+        xDifference = player.transform.position.x - transform.position.x;
+        yDifference = player.transform.position.y - transform.position.y;
+
+        // So these values will have to be changed once the two things get actual sprites. Also could probably be managed from a game manager? Not sure tho...
+        if(xDifference > -.5 && xDifference < .5) //Making sure the player x is within .5 units from the thisObject x
+        {
+            if (yDifference > -.5 && yDifference < .5) //Making sure the player y is within .5 units from the thisObject y
+            {
+                Debug.Log("This is like close enough to interact I guess.");
+            }
+        }
+
+        #region Proximity testing. Feel free to ignore :)
+        /*
+        if (xDifference > .5)
+        {
+            if (yDifference > .5)
+            {
+                Debug.Log("The player is to the right and above of the interactObject");
+            }
+            else if (yDifference < -.5)
+            {
+                Debug.Log("The player is to the right and below of the interactObject?");
+            }
+            else
+            {
+                Debug.Log("The player is to the right and at the same y as the interactObject");
+            }
+        }
+        else if (xDifference < -.5)
+        {
+            if (yDifference > .5)
+            {
+                Debug.Log("The player is to the left and above of the interactObject");
+            }
+            else if (yDifference < -.5)
+            {
+                Debug.Log("The player is to the left and below of the interactObject");
+            }
+            else
+            {
+                Debug.Log("The player is to the left and at the same y as the interactObject");
+            }
+        }
+        else
+        {
+            if (yDifference > .5)
+            {
+                Debug.Log("The player is above of the interactObject and at the same x");
+            }
+            else if (yDifference < -.5)
+            {
+                Debug.Log("The player is below of the interactObject and at the same x");
+            }
+            else
+            {
+                Debug.Log("The player is at the same x and y as the interactObject");
+            }
+        }
+        */
+        #endregion
     }
 
     public virtual void DoSomething()
--- OnHalloweenLight/Assets/Scripts/Player.cs
+++ OnHalloweenLight/Assets/Scripts/Player.cs
@@ -13,6 +13,16 @@ public class Player : Character
     // Update is called once per frame
     void Update()
     {
-        
+
+        //This will actually move the player
+        float yInput = Input.GetAxis("Vertical");
+        float xInput = Input.GetAxis("Horizontal");
+        transform.Translate(xInput * 10f * Time.deltaTime, yInput * 10f * Time.deltaTime, 0f);
+
+        //This is what locks the player on the screen. I'm not sure how needed this will be but might as well keep it for now.
+        Vector3 position = transform.position;
+        position.y = Mathf.Clamp(position.y, -5, 5);
+        position.x = Mathf.Clamp(position.x, -15, 15);
+        transform.position = position;
     }
 }
