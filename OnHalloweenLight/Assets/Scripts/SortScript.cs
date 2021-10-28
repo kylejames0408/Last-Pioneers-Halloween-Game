339b1954604ec563a292ff0ae5fe966bcd9b3e6e
awt1910 <awt1910@g.rit.edu>
2021-10-28 15:58:29 -0400

    fixed layering lol
    gottem

---
+++ OnHalloweenLight/Assets/Scripts/SortScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SortScript : MonoBehaviour
+{
+
+    public float yhax;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<SpriteRenderer>().sortingOrder = (Mathf.RoundToInt((transform.position.y + yhax) * 100f) * -1);
+    }
+}
